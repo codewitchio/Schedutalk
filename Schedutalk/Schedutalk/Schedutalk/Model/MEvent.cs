bd70a51a97e13f8ff88907fc025470f4ab20677b
Eric Lewerentz <Eric Lewerentz>
2016-05-18 17:05:34 +0200

    Add properties

--- Schedutalk/Schedutalk/Schedutalk/Model/MEvent.cs
+++ Schedutalk/Schedutalk/Schedutalk/Model/MEvent.cs
@@ -6,8 +6,46 @@ using System.Threading.Tasks;
 
 namespace Schedutalk.Model
 {
-    class MEvent : ViewModel.VMBase
+    public class MEvent : ViewModel.VMBase
     {
-        
+        public MDate StartDate
+        {
+            get;
+            set;
+        }
+
+        public MDate EndDate
+        {
+            get;
+            set;
+        }
+
+        string _title;
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+
+        string _information;
+        public string Information
+        {
+            get
+            {
+                return _information;
+            }
+            set
+            {
+                _information = value;
+                OnPropertyChanged("Information");
+            }
+        }
     }
 }
