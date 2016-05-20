fe912be4532027cd135bbb46cff767c4a0d37584
Eric Lewerentz <eric.lewerentz@hotmail.com>
2016-05-20 11:08:14 +0200

    Add missing file

---
+++ Schedutalk/Schedutalk/Schedutalk/View/ScheduleEventView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Schedutalk.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Schedutalk.View
+{
+    public partial class ScheduleEventView : ContentPage
+    {
+        public ScheduleEventView()
+        {
+            InitializeComponent();
+        }
+    }
+}
