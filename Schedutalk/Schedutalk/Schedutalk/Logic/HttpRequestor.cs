4c388da050675f773b8749fe7e348f88b4455283
Eric Lewerentz <Eric Lewerentz>
2016-05-24 22:18:18 +0200

    Implement utlity for httprequest as string

---
+++ Schedutalk/Schedutalk/Schedutalk/Logic/HttpRequestor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedutalk.Logic
+{
+    class HttpRequestor
+    {
+        public async Task<string> getHttpRequestAsString(Func<string, HttpRequestMessage> requestTask, string input)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpRequestMessage request = requestTask(input);
+            var response = httpClient.SendAsync(request);
+
+
+            var result = await response.Result.Content.ReadAsStringAsync();
+            return result;
+        }
+
+    }
+}
