5a5b4275168aa1de51e275cf0cf35f0416d8dc76
Eric Lewerentz <Eric Lewerentz>
2016-05-24 22:26:02 +0200

    commit changes in project folders

---
+++ Schedutalk/Schedutalk/Schedutalk/Logic/KTHPlaces/RoomExistsDataContract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schedutalk.Logic.KTHPlaces
+{
+    [DataContract]
+    class RoomExistsDataContract
+    {
+        [DataMember(Name = "exists")]
+        public bool Exists { get; set; }
+    }
+}
